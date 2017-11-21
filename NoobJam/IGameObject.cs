0585237f25cca54c7d427be1c66ab0435151fa3f
Hugo Peters <hpeters1024@gmail.com>
2017-11-21 17:41:12 +0100

    Framework

---
+++ NoobJam/IGameObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace NoobJam
+{
+    interface IGameObject
+    {
+        void Update();
+        void Draw(SpriteBatch batch, Camera cam);
+    }
+}
